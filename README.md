cc8343530e2377a8ed233854c72d0dabff2e0804
morpheuszein <morpheus.zein@irawan.id>
2023-10-08 22:58:16 +0700

    finishing touches

---
+++ README.md
@@ -0,0 +1,8 @@
+Hello, this is my first ever flutter project app, i made a Simple TodoLIst program.
+
+in this project ive learned many different things, such as:
+-how flutter components work
+-styling in flutter
+-how to use certain flutter functions Like setState
+
+ive learned all of these through the flutter docs, and youtube videos
---
+++ lib/screens/home.dart
@@ -0,0 +1,185 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/constants/colors.dart';
+import '/model/todo.dart';
+import 'package:myapp/widgets/todo_item.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final todosList = Todo.todoList();
+  List<Todo> _foundTodo = [];
+  final _todoController = TextEditingController();
+
+  @override
+  void initState() {
+    _foundTodo = todosList;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: tdBGcolor,
+      appBar: buildAppbar(),
+      body: Stack(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            child: Column(
+              children: [
+                searchbox(),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(top: 39, bottom: 20),
+                        child: const Text(
+                          "All toDos",
+                          style: TextStyle(
+                              fontSize: 30, fontWeight: FontWeight.w500),
+                        ),
+                      ),
+                      for (Todo todoo in _foundTodo.reversed)
+                        TodoItem(
+                          todo: todoo,
+                          onTodoChanged: _handleTodoChange,
+                          onDeleteItem: _deleteToDoItem,
+                        ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(children: [
+              Expanded(
+                  child: Container(
+                margin: const EdgeInsets.only(bottom: 20, right: 20, left: 20),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: const [
+                      BoxShadow(
+                          color: Colors.grey,
+                          offset: Offset(0.0, 0.0),
+                          blurRadius: 10.0,
+                          spreadRadius: 0.0)
+                    ],
+                    borderRadius: BorderRadius.circular(10)),
+                child: TextField(
+                  controller: _todoController,
+                  decoration: const InputDecoration(
+                      hintText: "Add new todo item", border: InputBorder.none),
+                ),
+              )),
+              Container(
+                margin: const EdgeInsets.only(bottom: 20, right: 20),
+                child: ElevatedButton(
+                  onPressed: () {
+                    _addToDoItem(_todoController.text);
+                  },
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: tdBlue, minimumSize: const Size(60, 60)),
+                  child: const Text(
+                    "+",
+                    style: TextStyle(fontSize: 40),
+                  ),
+                ),
+              )
+            ]),
+          )
+        ],
+      ),
+    );
+  }
+
+  void _handleTodoChange(Todo todo) {
+    setState(() {
+      todo.isDone = !todo.isDone;
+    });
+  }
+
+  void _deleteToDoItem(String id) {
+    setState(() {
+      todosList.removeWhere((element) => element.id == id);
+    });
+  }
+
+  void _runFilter(String enteredKeyword) {
+    List<Todo> results = [];
+    if (enteredKeyword.isEmpty) {
+      results = todosList;
+    } else {
+      results = todosList
+          .where((item) => item.todoText!
+              .toLowerCase()
+              .contains(enteredKeyword.toLowerCase()))
+          .toList();
+    }
+    setState(() {
+      _foundTodo = results;
+    });
+  }
+
+  void _addToDoItem(String todo) {
+    setState(() {
+      todosList.add(Todo(
+          id: DateTime.now().millisecondsSinceEpoch.toString(),
+          todoText: todo));
+    });
+    _todoController.clear();
+  }
+
+  Widget searchbox() {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(20)),
+      child: TextField(
+        onChanged: (value) => _runFilter(value),
+        decoration: const InputDecoration(
+            contentPadding: EdgeInsets.all(10),
+            prefixIcon: Icon(
+              Icons.search,
+              color: tdBlack,
+              size: 20,
+            ),
+            prefixIconConstraints: BoxConstraints(maxHeight: 20, minWidth: 25),
+            border: InputBorder.none,
+            hintText: "Search",
+            hintStyle: TextStyle(color: tdGrey)),
+      ),
+    );
+  }
+
+  AppBar buildAppbar() {
+    return AppBar(
+      elevation: 0,
+      backgroundColor: tdBGcolor,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Icon(
+            Icons.menu,
+            color: tdBlack,
+            size: 30,
+          ),
+          SizedBox(
+            height: 40,
+            width: 40,
+            child: ClipRRect(
+                borderRadius: BorderRadius.circular(100),
+                child: Image.asset('assets/images/MURPHY.jpg')),
+          )
+        ],
+      ),
+    );
+  }
+}
